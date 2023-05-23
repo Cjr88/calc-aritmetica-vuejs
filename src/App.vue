edd6c631bf78021712c1a531b477ef503ce001cc
Cjr88 <carloscjr70@gmail.com>
2023-05-23 18:35:50 -0300

    conclusão da logica de operações aritméticas

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
+<!-- eslint-disable vue/require-v-for-key -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <header class="p-5">
+      <h1>Calculadora Aritmética</h1>
+    </header>
+    <form>
+      <div class="row " >
+        <div class=" col-md-3 text-center ">
+              <!-- <select class="form-control m-1" >
+            <option>Escolha a operação</option>
+            <option value="subtracao" @click="calcularSubtracao">Subtração</option>
+            <option value="multiplicacao"  @click="calcularMultiplicacao" >Multiplicação</option>
+            <option value="divisao" @click="calcularDivisao">Divisão</option>
+            <option value="adicao" @click="calcularAdicao">Adição</option>
+          </select> -->
+        </div>
+        <div class="col-md-6 d-flex mb-5" >
+          <input @keyup="evento => estado.numero1 = evento.target.value" class="form-control m-1" />
+          <input  @keyup="evento => estado.numero2 = evento.target.value" class="form-control m-1" />  
+        </div>
+      </div>
+    </form>
+    <!-- <ul class="list-group mt-4">
+      <li class="list-group-item"  v-for="calculo in estado.calculos ">
+        <label class="ms-3" >
+          {{ calculo.titulo }} 
+          {{ calculo.calcularMultiplicacao }}
+          {{ calculo.calcularSubtracao }}
+          {{ calculo.calcularDivisao }}
+          {{  estado.numero1 * estado.numero2  }} 
+        </label>
+      </li>
+    </ul> -->
+          Multiplicação: {{ calcularMultiplicacao() }} <br>
+          Subtração: {{ calcularSubtracao() }} <br>
+          Divisão: {{ calcularDivisao() }} <br>
+          Adição: {{ calcularAdicao() }} <br>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+  import { reactive } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ 
+  const  estado = reactive({ 
+    calculos: 
+    [
+    {
+      numero1: '',
+      numero2: '',
+      // titulo: 'Mutiplicação:',
+      // calcularMultiplicacao:  numero1 * numero2, 
+    },
+    // {
+    //   titulo: 'Subtração:',
+    //   // calcularSubtracao: numero1 - numero2,
+    // },
+    // {
+    //   titulo: 'Divisão:',
+    //   // calcularDivisao: numero1 / numero2,
+    // },
+    ]
+  })
+  function calcularMultiplicacao(){
+const { numero1, numero2 } = estado;
+  return numero1 * numero2;
+}
+function calcularSubtracao(){
+const { numero1, numero2 } = estado;
+  return numero1 - numero2;
+}
+function calcularDivisao(){
+const { numero1, numero2 } = estado;
+  return numero1 / numero2;
+}
+function calcularAdicao(){
+const { numero1, numero2 } = estado;
+  return parseInt(numero1) + parseInt(numero2);
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.container {
+  width: 320px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: brown;
+  font-size: 26px;
+}h1{
+  letter-spacing: 11px;
+  
 }
 </style>
